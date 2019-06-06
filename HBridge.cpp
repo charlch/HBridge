93649b2e2ab6327dca4d841058f5714976816f7f
Chris Charles <charlch@users.noreply.github.com>
2019-06-06 22:42:51 +0100

    Update HBridge.cpp

--- HBridge.cpp
+++ HBridge.cpp
@@ -7,6 +7,19 @@ HBridge::HBridge(int pinA, int pinB, float dead_band_fct)
   pinMode(pinB, OUTPUT);
   _pinA = pinA;
   _pinB = pinB;
+  _pinEn = -1;
+  _dead_band_fct = dead_band_fct;
+  set_signal(0.0);
+}
+
+HBridge::HBridge(int pinA, int pinB, int pinEn, float dead_band_fct)
+{
+  pinMode(pinA, OUTPUT);
+  pinMode(pinB, OUTPUT);
+  pinMode(pinEn, OUTPUT);
+  _pinA = pinA;
+  _pinB = pinB;
+  _pinEn = pinEn;
   _dead_band_fct = dead_band_fct;
   set_signal(0.0);
 }
@@ -19,15 +32,27 @@ void HBridge::set_signal(float signal)
     /* Braking */
     signal = 0;
   }
+  
+  int inSig;
+
+  if (_pinEn > -1)
+  {
+    analogWrite(_pinEn, (int)255*signal);
+    inSig = 255;
+  }
+  else
+  {
+    inSig = (int)255*signal;
+  }
 
   if (signal > 0)
   {
-    analogWrite(_pinA, (int)255*signal);
+    analogWrite(_pinA, inSig);
     analogWrite(_pinB, 0);
   }
   else
   {
-    analogWrite(_pinB, (int)255*signal);
+    analogWrite(_pinB, inSig);
     analogWrite(_pinA, 0);
   }
 }
