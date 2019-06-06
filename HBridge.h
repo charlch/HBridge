1cd5d4a701b0b571dc12749087ce17941a8aece3
Chris Charles <charlch@users.noreply.github.com>
2019-06-06 22:31:23 +0100

    Update HBridge.h

--- HBridge.h
+++ HBridge.h
@@ -7,10 +7,12 @@ class HBridge
 {
   public:
     HBridge(int pinA, int pinB, float dead_band_fct);
+    HBridge(int pinA, int pinB, int pinEn, float dead_band_fct);
     void set_signal(float signal);
   private:
     int _pinA;
     int _pinB;
+    int _pinEn;
     float _dead_band_fct;
 };
 
